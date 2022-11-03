5052fab4ff6580aa8603c94674e83f82338bad42
chiheb <chiheb.fellah@gmail.com>
2022-11-03 21:26:07 +0100

    directives

--- src/app/notification/notification.component.ts
+++ src/app/notification/notification.component.ts
@@ -2,13 +2,14 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-notification',
-  template: `<div [hidden]="hideNotification"  class="alert alert-success">
+  template: `<div class="alert alert-success" [ngClass]="{fadeOut:hideNotification}">
                    This website uses cookies to provide better user experience.
-                   <div (click)="closeNotification()" class="close"><button class="btn">X</button></div>
+                   <div class="close"><button class="btn" (click)="closeNotification()">X</button></div>
               </div>`,
   styles: [".notification-div{margin: 10px 0px;padding: 10px 20px;background-color: #FAD7A0;text-align:center;}",
 "p{font-size:14px}",
-".close{float:right;margin-top:-15px;}"]
+".close{float:right;margin-top:-15px;}",
+".fadeOut{visibility:hidden;opacity:0;transition: visibility 0s 2s,opacity 2s linear;}"]
 })
 export class NotificationComponent implements OnInit {
 
